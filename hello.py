dd98dbca66d3fe3032ac42405cc1cee2766d0ff2
greydog8 <siebgrey@gmail.com>
2014-07-07 20:11:06 -0400

    first code

---
+++ hello.py
@@ -0,0 +1,23 @@
+print("hello World")
+
+print("hi")
+
+name = input("what do you do?")
+
+name2 = input("what is your name")
+
+print("your name is " + name2 + " nice to meet you")
+
+name3 = input ("what is the most rare thing in minecraft")
+if name3== "dimonds":
+    print("you are right good job")
+
+elif name3== "emerlads":
+
+    print("nice try")
+
+else:
+
+    print("you are worng the aswner is dimonds")
+
+
