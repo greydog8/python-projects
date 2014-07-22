b9ee1577746d1923654af8f689ff73ab273340cb
greydog8 <siebgrey@gmail.com>
2014-07-21 20:03:05 -0400

    my test

---
+++ test.py
@@ -0,0 +1,13 @@
+print("hello world")
+
+aswner = input("what is your name?")
+
+print("hi " + aswner)
+
+years_old = input("how old are you?")
+
+print("oh you are " + years_old)
+
+years_old = int(years_old)
+
+print("did you know " + str(years_old) + "is " + str(years_old * 365))
