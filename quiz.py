c279b41e31f665a397aaf6cb4a3f38672533a258
greydog8 <siebgrey@gmail.com>
2014-08-04 21:06:28 -0400

    quize

---
+++ quiz.py
@@ -0,0 +1,14 @@
+print("welcome to loremaster of minecraft")
+
+while True:
+    aswner = input("are you ready")
+
+    if aswner == "yes":
+        print ("ok lets go")
+        aswner2 = input("how do you make a crafting table in minecraft")
+        if aswner2 == "i dont know":
+         print("oh ")    
+        if aswner2 == "you put 4 wood planks in a 2 by 2 grid":
+         print("good job")
+         if aswner2 =="you put 4 wood planks in a 2 by 2 grid":
+          print("hi")    
