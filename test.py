273ad7a236e57b09d83d4b87f6d355f595b770fc
greydog8 <siebgrey@gmail.com>
2014-08-11 20:00:13 -0400

    death brige

---
+++ test.py
@@ -0,0 +1,11 @@
+print('''
+who would cross the brige of death mus aswner me thees
+questions three, 'ere the other side he see
+''')
+
+name = input ("what is your name")
+
+name = name.strip().lower()
+
+if name.startswith('lancelot'):
+    print ("ok")
